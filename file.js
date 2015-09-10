8c311de53bb4762a6a5f39213ca633e1f5c4821a
Alejandro Londono <adlondono@elm-d-10002329.corp.ericksonretirement.com>
2015-09-10 14:12:00 -0400

    created ability to encode and decode variables over the url, also maintains multiple rect types

--- file.js
+++ file.js
@@ -22,58 +22,107 @@ canvas.on('object:scaling', function(){
 function nameSpacing(obj){
 	return obj.fill+'_'+obj.type+Math.round((10000*Math.random()));
 }
-function newCouch(){
+function newCouch(x,y){
 	// event.preventDefault();
+	type = $('select option:selected').text();
+	var fill;
+	if(type == 'Chair'){
+		fill = 'blue';
+	}else if(type == 'Couch'){
+		fill = 'green';
+	}else{
+		fill = 'red';
+	}
 	count++;
 	rect= new fabric.Rect({
   left: 100,
   top: 100,
-  fill: 'red',
+  fill: fill,
   width: 20,
   height: 20,
-  
 });
 	rect.name = nameSpacing(rect);
 	rects.push(rect);
 	console.log(rects);
 	canvas.add(rect).setActiveObject(rect);
 }
-rects.ajaxGet =  function(){
-	$.get('fetch.php',function(data){
-		console.log(data);
-	});
+function oldCouch(x,y,xScale,yScale,angle,type){
+	// event.preventDefault();
+	if(type == 'blue'){
+		fill = 'blue';
+	}else if(type == 'green'){
+		fill = 'green';
+	}else{
+		fill = 'red';
+	}
+	count++;
+	rect= new fabric.Rect({
+  left: x,
+  top: y,
+  scaleX: xScale,
+  scaleY: yScale,
+  fill: fill,
+  angle: angle,
+  width: 20,
+  height: 20,
+});
+	rect.name = nameSpacing(rect);
+	rects.push(rect);
+	console.log(rects);
+	canvas.add(rect).setActiveObject(rect);
+}
+rects.recieveUrlParams = function(){
+
+	var query = window.location.search.substring(1);
+       var queries = query.split("-");
+       queries.pop();
+       console.log(queries);
+				for(var k=0; k<queries.length;k++){
+
+					vars =  queries[k].split('&');
+				
+					console.log(vars);
+					var params=[];
+					for(var i=0;i<vars.length;i++){
+
+						if(isNaN(parseFloat(vars[i].replace(/\b\w+=/,'')))){
+							console.log('success');
+							params[i] = vars[i].replace(/\b\w+=/,'');
+						}else{
+							console.log('whoops');
+							params[i] = parseFloat(vars[i].replace(/\b\w+=/,''));
+						}
+						console.log(params);
+						
+					}
+					oldCouch(params[0],params[1],params[2],params[3],params[4],params[5]);
+				}
+       
+					 
 };
-rects.ajaxGet();
-rects.urlParams = function(rect){
+rects.recieveUrlParams();
+rects.processUrlParams= function(){
+
+};
+rects.sendUrlParams = function(rect){
+	var query = "index.php?";
 	for(var i=0; i<rects.length; i++){
 		
-		var data = {
-		x: this[i].left,
-		y: this[i].top,
-		xScale: this[i].scaleX,
-		yScale: this[i].scaleY,
-		rotation: this[i].angle,
-		type: this[i].type,
-		name: this[i].name
-		};
-
-		$.ajax({
-		url: 'file.php',
-		cache: false,
-		type: 'POST',
-		dataType: 'html',
-		data: data,
-		success: function(returnData) {
-       console.log(returnData);
-       console.log('success');
-    },
-		error: function(){
-				console.log('nope');
-		}
-			});
-	console.log(data);
+		
+		// var x=this[i].left;
+		// var y=this[i].top;
+		// var xScale=this[i].scaleX;
+		// var yScale=this[i].scaleY;
+		// var rotation=this[i].angle;
+		// var type=this[i].type;
+		// var name=this[i].name;
+		
+		query += "x"+i+"="+this[i].left+"&y"+i+"="+this[i].top+"&xScale"+i+"="+this[i].scaleX+"&yScale"+i+"="+this[i].scaleY+"&angle"+i+"="+this[i].angle+"&type"+i+"="+this[i].fill+"&name"+i+"="+this[i].name+"-";
+		console.log(query);
+		window.location.href =query;
+		console.log(window.location.href);
+		// create query string for url
 	}
-	
 
 };
 
@@ -81,7 +130,7 @@ rects.urlParams = function(rect){
 
 	$('.data').on('click', function(){
 		event.preventDefault();
-		rects.urlParams();
+		rects.sendUrlParams();
 
 	});
 });
